f42dd4b7fed7b2258134f75f99e329b61f373062
RocketRaccoon516 <smosher@knights.ucf.edu>
2017-03-23 13:50:38 -0400

    This connects all the programs

---
+++ pl0compiler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Programs
+#include "pl0.c"
+#include "scanner.c"
+#include "parser5.c"
+
+void main()
+{
+    //Main functions of programs that are scanner, parser, and vm respectively.
+    main2();
+    main3();
+    main1();
+}
