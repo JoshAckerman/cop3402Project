551a923e804415f778088928d26762b32033a6c7
RocketRaccoon516 <smosher@knights.ucf.edu>
2017-03-25 23:12:26 -0400

    Partial code for compiler directives in comments

--- pl0compiler.c
+++ pl0compiler.c
@@ -1,14 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //Programs
-#include "pl0.c"
-#include "scanner.c"
-#include "parser5.c"
 
-void main()
+#include "scanner.c"
+#include "COP3402HW3.c"
+#include "pl0.c"
+
+
+int main( /*int argc, char *argv[]*/ )
 {
+    int printScanner=0;
+    int printParser=0;
+    int printVM=0;
+    /*if(argv[1])
+    {
+        if(strcmp(argv[1],"-l")==0)
+        {
+             printScanner=1;
+        }
+        else if(strcmp(argv[1],"-a")==0)
+        {
+            printParser=1;
+        }
+        else if(strcmp(argv[1],"-v")==0)
+        {
+
+        }
+    }
+    if(argv[2])
+    {
+        if(strcmp(argv[2],"-l")==0)
+        {
+            printScanner=1;
+        }
+        else if(strcmp(argv[2],"-a")==0)
+        {
+            printParser=1;
+        }
+        else if(strcmp(argv[2],"-v")==0)
+        {
+
+        }
+    }
+    if(argv[3])
+    {
+        if(strcmp(argv[3],"-l")==0)
+        {
+            printScanner=1;
+        }
+        else if(strcmp(argv[3],"-a")==0)
+        {
+            printParser=1;
+        }
+        else if(strcmp(argv[3],"-v")==0)
+        {
+
+        }
+    }*/
+    //printf("i");
     //Main functions of programs that are scanner, parser, and vm respectively.
+    //main2(printScanner);
     main2();
+    //printf("%s",pArray[0]);
     main3();
+    //printf("o");
+    //main3(printParser);
+    //printf("p");
     main1();
 }
